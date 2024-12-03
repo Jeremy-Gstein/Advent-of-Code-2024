72f7c4e9fc044c71c70fea5b5c047b80eced180a
Jeremy-Gstein <jeremy51b5@protonmail.com>
2024-12-02 20:05:49 -0500

    add notes to day1. need to finish finiding differences.

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -15,18 +15,32 @@ fn read_lines(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
             chars.sort_by(|a, b| b.cmp(a));
             chars.reverse();
             let sorted1: String = chars.into_iter().collect();
+            decode_difference(&sorted1);
             
             let sorted2 = sort_numbers(parts[1]);
             let mut chars: Vec<char> = sorted2.chars().collect();
             chars.sort_by(|a, b| b.cmp(a));
             chars.reverse();
             let sorted2: String = chars.into_iter().collect();
+            decode_difference(&sorted2);
             // TODO: add sorted1[n] sorted2[n] 
             //      where n is an iterator of the length of each set.
             println!("{} {}", sorted1, sorted2);
         }
     }
     Ok(())
+}
+
+
+// take a reference to a string of numbers example "312142" and return a single byte.
+fn decode_difference(sorted_string: &str) -> char {
+    let sorted_string = sorted_string;
+    let bytes = sorted_string.as_bytes();
+    let first_byte: u32 = bytes[0].into();
+    let first_byte_decodes = char::from_u32(first_byte).unwrap();
+    println!("Byte 1: {}", first_byte_decodes);
+    return first_byte_decodes;
+            
 }
 
 
---
+++ day1/target/debug/deps/day1-b0d4b33e470dd956.d
@@ -0,0 +1,5 @@
+/home/jg/Advent-of-Code-2024/day1/target/debug/deps/libday1-b0d4b33e470dd956.rmeta: src/main.rs
+
+/home/jg/Advent-of-Code-2024/day1/target/debug/deps/day1-b0d4b33e470dd956.d: src/main.rs
+
+src/main.rs:
