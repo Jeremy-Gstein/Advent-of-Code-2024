447e5ac6971187fafcac5ec6006457bb2a8f1f93
Jeremy-Gstein <jeremy51b5@pm.me>
2024-12-03 15:32:14 -0500

    day1 *wrong*)

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -5,9 +5,11 @@ use std::io::{self, BufReader, BufRead};
 fn read_lines(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
-
+    let index: usize = 0;
+    let mut day1_answer: i32 = 0;
     for line in reader.lines() {
         let line = line?;
+        let mut some_sum: i32 = 0;
         let parts: Vec<&str> = line.split_whitespace().collect();
         if parts.len() == 2 {
             let sorted1 = sort_numbers(parts[0]);
@@ -15,30 +17,45 @@ fn read_lines(file_path: &str) -> Result<(), Box<dyn std::error::Error>> {
             chars.sort_by(|a, b| b.cmp(a));
             chars.reverse();
             let sorted1: String = chars.into_iter().collect();
-            decode_difference(&sorted1);
             
             let sorted2 = sort_numbers(parts[1]);
             let mut chars: Vec<char> = sorted2.chars().collect();
             chars.sort_by(|a, b| b.cmp(a));
             chars.reverse();
             let sorted2: String = chars.into_iter().collect();
-            decode_difference(&sorted2);
-            // TODO: add sorted1[n] sorted2[n] 
-            //      where n is an iterator of the length of each set.
-            println!("{} {}", sorted1, sorted2);
+
+            assert!(sorted1.len() == sorted2.len()); 
+            for index in 0..sorted1.len() {
+                let num1: char = decode_difference(index, &sorted1);
+                let num2: char = decode_difference(index, &sorted2);
+                let diff: i32 = (num1 as i32 - num2 as i32).abs();
+                some_sum += diff;
+                println!("{} - {} = {}", num1, num2, diff);
+            }            
+            
+            println!("Total: {}", some_sum);
+            println!("Origin: {} {}", parts[0], parts[1]);
+            println!("Sorted: {} {}", sorted1, sorted2);
+            //dayday1_answer += some_sum;
+            println!("{} += {}", day1_answer, some_sum);
+            day1_answer += some_sum;
         }
+        //day1_answer += some_sum;
     }
+    println!("Finger Crossed: {}", day1_answer);
     Ok(())
 }
 
-
 // take a reference to a string of numbers example "312142" and return a single byte.
-fn decode_difference(sorted_string: &str) -> char {
-    let sorted_string = sorted_string;
+fn decode_difference(index: usize, sorted_string: &str) -> char {
+    //let sorted_string = sorted_string;
     let bytes = sorted_string.as_bytes();
-    let first_byte: u32 = bytes[0].into();
+    if index >= bytes.len() {
+        panic!("panic at the byte code");
+    }
+    let first_byte: u32 = bytes[index] as u32;
     let first_byte_decodes = char::from_u32(first_byte).unwrap();
-    println!("Byte 1: {}", first_byte_decodes);
+    //println!("Byte 1: {}", first_byte_decodes);
     return first_byte_decodes;
             
 }
@@ -53,7 +70,7 @@ fn sort_numbers(numbers: &str) -> String {
 }
 
 fn main() -> io::Result<()> {
-    read_lines("list.txt").expect("ERROR");
+    read_lines("input.txt").expect("ERROR");
 
     Ok(())
 }
